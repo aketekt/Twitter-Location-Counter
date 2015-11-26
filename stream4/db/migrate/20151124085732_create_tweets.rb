154a86c6e45d2a46bfaad839f42af6f2b8973c3d
Will <will.sch@protonmail.com>
2015-11-26 08:03:24 +0000

    first commit

--- stream4/db/migrate/20151124085732_create_tweets.rb
+++
@@ -1,9 +0,0 @@
-class CreateTweets < ActiveRecord::Migration
-  def change
-    create_table :tweets do |t|
-      t.text :text
-      t.string :location
-      t.timestamps null: false
-    end
-  end
-end
